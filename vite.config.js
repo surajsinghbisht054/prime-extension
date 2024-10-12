953ef3545975e1d702189e491499773817e01514
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2024-10-12 10:59:00 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: 'static/primeflex-extension/assets/[name].js',  // For JavaScript
-        chunkFileNames: 'static/primeflex-extension/assets/[name].js',  // For JS chunks
-        assetFileNames: 'static/primeflex-extension/assets/[name].[ext]', // For CSS and other assets
-      }
+        entryFileNames: '[name].js',  // For JavaScript
+        chunkFileNames: '[name].js',  // For JS chunks
+        assetFileNames: '[name].[ext]', // For CSS and other assets
+      },
+      input: 'src/main.js'
     }
   }
 });
